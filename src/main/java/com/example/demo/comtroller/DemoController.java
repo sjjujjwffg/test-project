e1715dfd430f0d51feeae1c61a69e951c0f5b6b5
东软-覃章佳 <836747985@qq.com>
2021-08-31 10:15:37 +0800

    1. first time committed.

---
+++ src/main/java/com/example/demo/comtroller/DemoController.java
@@ -0,0 +1,20 @@
+package com.example.demo.comtroller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * @Author: qinzhangjia
+ * @Date: 2021/8/31 10:05
+ * @Description:
+ */
+@RestController
+public class DemoController {
+
+    @RequestMapping("/api/view")
+    public Object index(){
+        return "Tested by time："+new Date();
+    }
+}
